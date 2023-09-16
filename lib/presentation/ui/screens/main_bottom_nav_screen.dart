c440227e8b0e12c877a245b52075416c6aef1b2d
fakhruddin07 <fakhruddinnoman95@gmail.com>
2023-09-16 12:34:13 +0600

    bottom nav bar ui design added

---
+++ lib/presentation/ui/screens/main_bottom_nav_screen.dart
@@ -0,0 +1,49 @@
+import 'package:ecommerce_with_flutter/presentation/ui/screens/home_screen.dart';
+import 'package:ecommerce_with_flutter/presentation/ui/utility/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class MainBottomNavScreen extends StatefulWidget {
+  const MainBottomNavScreen({super.key});
+
+  @override
+  State<MainBottomNavScreen> createState() => _MainBottomNavScreenState();
+}
+
+class _MainBottomNavScreenState extends State<MainBottomNavScreen> {
+  int _selectedIndex = 0;
+  final List<Widget> _screen = const [
+    HomeScreen(),
+    HomeScreen(),
+    HomeScreen(),
+    HomeScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screen[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        showUnselectedLabels: true,
+        selectedItemColor: AppColors.primaryColor,
+        unselectedItemColor: Colors.grey,
+        elevation: 4,
+        currentIndex: _selectedIndex,
+        onTap: (index) {
+          _selectedIndex = index;
+          if (mounted) {
+            setState(() {});
+          }
+        },
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home_filled), label: "Home"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.category), label: "Categories"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_cart), label: "Cart"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.favorite_border), label: "Wishlist"),
+        ],
+      ),
+    );
+  }
+}
--- lib/presentation/ui/screens/splash_screen.dart
+++ lib/presentation/ui/screens/splash_screen.dart
@@ -1,5 +1,4 @@
-import 'package:ecommerce_with_flutter/presentation/ui/screens/auth/email_verification_screen.dart';
-import 'package:ecommerce_with_flutter/presentation/ui/screens/home_screen.dart';
+import 'package:ecommerce_with_flutter/presentation/ui/screens/main_bottom_nav_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
@@ -22,8 +21,7 @@ class _SplashScreenState extends State<SplashScreen> {
 
   void goToNextScreen() {
     Future.delayed(const Duration(seconds: 3)).then((value) {
-      Get.offAll(const HomeScreen());
-      // Get.offAll(const EmailVerificationScreen());
+      Get.offAll(const MainBottomNavScreen());
     });
   }
 
